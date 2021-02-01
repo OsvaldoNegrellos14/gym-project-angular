f360490bf105be566b6f13617d883e7e66d7d7f8
Xevantt <xevantt28@gmail.com>
2021-02-01 00:31:02 -0600

    Modificación de carrusel - CSS y HTML, Probando ruta About Us

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutUsComponent } from './page/about-us/about-us.component';
 import {IndexComponent} from './page/index/index.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
-  { path: 'home', component: IndexComponent }
+  { path: 'home', component: IndexComponent },
+  { path: 'aboutus', component: AboutUsComponent}
 ];
 
 @NgModule({
