ddd9b6a08ff241ee76a9ebe6828ae908fe83793b
Javier Gómez Cohetero <cohetero745@gmail.com>
2021-03-08 18:50:54 -0600

    validación de formulario sign in

--- src/app/page/sign-in/sign-in.component.ts
+++ src/app/page/sign-in/sign-in.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { FireService } from 'src/app/services/fire.service';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
@@ -16,7 +16,11 @@ export class SignInComponent implements OnInit {
     email: [''],
     password: ['']
   });
-  constructor(public firebaseService: FireService, private form: FormBuilder, private route: Router, private modalService: BsModalService) { }
+  public contactForm: FormGroup;
+  emailPattern: any = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  constructor(public firebaseService: FireService, private form: FormBuilder, private route: Router, private modalService: BsModalService) {
+    this.contactForm = this.validation();
+  }
 
   ngOnInit() {
     if (localStorage.getItem('user') !== null) {
@@ -25,20 +29,31 @@ export class SignInComponent implements OnInit {
       this.isSignedIn = false;
     }
   }
-
+  get email() { return this.contactForm.get('email'); }
+  get password() { return this.contactForm.get('password'); }
+  validation() {
+    return this.user = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.pattern(this.emailPattern)]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    });
+  }
   async onSignUp(template: TemplateRef<any>) {
-    await this.firebaseService.signUp(this.user.value.email, this.user.value.password);
-    if (this.firebaseService.isLoggin) {
-      this.openModal(template);
-      this.isSignedIn = true;
+    if (this.contactForm.valid) {
+      await this.firebaseService.signUp(this.user.value.email, this.user.value.password);
+      if (this.firebaseService.isLoggin) {
+        this.openModal(template);
+        this.isSignedIn = true;
+      }
     }
   }
 
   async onSignIn() {
-    await this.firebaseService.signIn(this.user.value.email, this.user.value.password);
-    if (this.firebaseService.isLoggin) {
-      this.isSignedIn = true;
-      this.route.navigateByUrl('admin');
+    if (this.contactForm.valid) {
+      await this.firebaseService.signIn(this.user.value.email, this.user.value.password);
+      if (this.firebaseService.isLoggin) {
+        this.isSignedIn = true;
+        this.route.navigateByUrl('admin');
+      }
     }
   }
   openModal(template: TemplateRef<any>) {
