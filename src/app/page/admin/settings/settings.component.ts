6d77dab28025e46d3c8e3127cf31c46b79c3edc1
Xevantt <xevantt28@gmail.com>
2021-03-14 15:12:52 -0600

    Modal para CRUD de coach en settings

--- src/app/page/admin/settings/settings.component.ts
+++ src/app/page/admin/settings/settings.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
+  preview = true;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  confirm() {
+    console.log("Routine deleted");
+  }
+
+  decline() {
+    console.log("Operation canceled");
   }
 
 }
